8b4fc82caf28ef145477b8a2dc13731b6fe3652e
ismail <kasaliismail2@gmail.com>
2024-04-14 14:53:33 +0100

    first commit

---
+++ src/main/java/com/mymavenprojects/App.java
@@ -0,0 +1,13 @@
+package com.mymavenprojects;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Alhamdulillahi alaa ni'imatul-islaam." );
+    }
+}
